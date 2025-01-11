82748b96452a2f70c40e0f34c362d77fdcf84f49
YSPlop <sivarajyukash@gmail.com>
2025-01-12 00:49:20 +1100

    pakho is getting a warmup

---
+++ src/components/Hero.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import styles from "../styles/hero.module.css";
+
+const Hero = () => {
+
+    return (
+        <div className={styles.backgroundColor}>
+            
+        </div>
+    );
+
+};
+export default Hero;
